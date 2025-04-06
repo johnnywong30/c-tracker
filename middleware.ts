460865286f128def1c15bf792eaddf7d9bbcf1cc
Johnny Wong <johnny.wong2324@gmail.com>
2025-04-05 23:30:17 -0400

    use middleware to handle public paths unauthenticated

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,40 @@
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-import { auth0 } from "./lib/auth0";
+import { auth0 } from "@/lib/auth0";
+
+const PUBLIC_PATHS = ["/"];
 
 export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+  const pathname = request.nextUrl.pathname;
+
+  const authRes = await auth0.middleware(request);
+
+  const session = await auth0.getSession(request);
+
+  if (PUBLIC_PATHS.includes(pathname)) {
+    if (session && pathname === "/") {
+      return NextResponse.redirect(
+        new URL("/dashboard", request.nextUrl.origin)
+      );
+    }
+
+    return NextResponse.next();
+  }
+
+  if (pathname.startsWith("/auth")) {
+    return authRes;
+  }
+
+  if (!session) {
+    // user is not authenticated, redirect to login page
+    return NextResponse.redirect(
+      new URL("/auth/login", request.nextUrl.origin)
+    );
+  }
+
+  // the headers from the auth middleware should always be returned
+  return authRes;
 }
 
 export const config = {
